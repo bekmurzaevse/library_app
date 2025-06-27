86f38a1073590a346c907743342df725da8b3e53
bekmurzaevse <bekmurzaevse@gmail.com>
2025-06-27 12:57:35 +0500

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    Cache::remember('gilt', 600, function () {
-        return User::all();
-    });
     return view('welcome');
 });
 
